a904bfb6c62fcfb5ec0a581e15551029cce7d063
kauekaue-cyber <kauekaue@gmail.com>
2025-12-06 17:34:14 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: false
-  }
-}
-module.exports = nextConfig
+const nextConfig = {};
+module.exports = nextConfig;
